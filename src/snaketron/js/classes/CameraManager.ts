4b65b5a5f15707907b83f1b2ecf057d19ee0c4e9
David McQueen <david@dmcqueen.co.uk>
2020-04-16 20:30:22 +0100

    Create CameraManager

---
+++ src/snaketron/js/classes/CameraManager.ts
@@ -0,0 +1,194 @@
+import CameraHint from "./CameraHint";
+
+interface IScoreboxConfig {
+    scene: Phaser.Scene
+}
+
+class CameraManager extends Phaser.GameObjects.Container {
+    
+    private cameraHints: Phaser.GameObjects.Group;
+    private camera: Phaser.Cameras.Scene2D.Camera;
+
+    constructor(config: IScoreboxConfig, camera: Phaser.Cameras.Scene2D.Camera){
+        super(config.scene)
+
+        this.scene = config.scene;
+        this.camera = camera;
+
+        // Add camera hints before we start moving the camera around with 'follow'
+        this.cameraHints = this.scene.add.group();
+        this.addCameraHints();
+    }
+
+    private addCameraHints = () => {
+
+        const hints = [
+        { // TR Corner Horizontal
+            x: 400,
+            y: 0,
+            width: 100,
+            height: 5,
+            alpha: 0.5,
+            startDeg: -60,
+            endDeg: -30
+        },
+        { // TR Corner Vertical
+            x: 495,
+            y: 0,
+            width: 5,
+            height: 100,
+            alpha: 0.5,
+            startDeg: -60,
+            endDeg: -30
+        },
+        { // Right
+            x: 495,
+            y: 150,
+            width: 5,
+            height: 200,
+            alpha: 0.5,
+            startDeg: -30,
+            endDeg: 30
+        },
+        { // BR Corner Horizontal
+            x: 400,
+            y: 495,
+            width: 100,
+            height: 5,
+            alpha: 0.5,
+            startDeg: 30,
+            endDeg: 60
+        },
+        { // BR Corner Vertical
+            x: 495,
+            y: 400,
+            width: 5,
+            height: 100,
+            alpha: 0.5,
+            startDeg: 30,
+            endDeg: 60
+        },
+        { // Bottom
+            x: 150,
+            y: 495,
+            width: 200,
+            height: 5,
+            alpha: 0.5,
+            startDeg: 60,
+            endDeg: 120
+        },
+        { // BL Corner Horizontal
+            x: 0,
+            y: 495,
+            width: 100,
+            height: 5,
+            alpha: 0.5,
+            startDeg: 120,
+            endDeg: 150
+        },
+        { // BL Corner Vertical
+            x: 0,
+            y: 400,
+            width: 5,
+            height: 100,
+            alpha: 0.5,
+            startDeg: 120,
+            endDeg: 150
+        },
+        { // Left
+            x: 0,
+            y: 150,
+            width: 5,
+            height: 200,
+            alpha: 0.5,
+            startDeg: 150, // TODO:- 
+            endDeg: -150, // TODO:- 
+            bounds: true
+        },
+        {
+            // TL Corner Horizontal
+            x: 0,
+            y: 0,
+            width: 100,
+            height: 5,
+            alpha: 0.5,
+            startDeg: -150,
+            endDeg: -120
+        },{
+            // TL Corner Vertical
+            x: 0,
+            y: 0,
+            width: 5,
+            height: 100,
+            alpha: 0.5,
+            startDeg: -150,
+            endDeg: -120
+        },
+        { // Top
+            x: 150,
+            y: 0,
+            width: 200,
+            height: 5,
+            alpha: 0.5,
+            startDeg: -120,
+            endDeg: -60
+        }];
+
+        hints.forEach(hint => {
+            const hintRect = this.scene.add.rectangle(hint.x, hint.y, hint.width, hint.height, 0x36ff14, 0.75).setScrollFactor(0).setOrigin(0, 0) as CameraHint;
+            hintRect.StartDeg = hint.startDeg;
+            hintRect.EndDeg = hint.endDeg;
+            hintRect.goToBounds = hint.bounds;
+            hintRect.setVisible(false);
+            this.cameraHints.add(hintRect);
+        });
+    }
+
+    // TODO:- Make generic
+    private objectInCameraViewport = (obj: any): boolean => {
+        const x = this.camera.midPoint.x - 250;
+        const width = this.camera.midPoint.x + 250;
+        
+        const y = this.camera.midPoint.y - 250;
+        const height = this.camera.midPoint.y + 250;
+
+        const bounds = new Phaser.Geom.Rectangle(x, y, width, height);
+        return bounds.contains(obj.x, obj.y);
+    }
+
+    private showCameraHints = (show: boolean) => {
+        this.cameraHints.children.iterate((part: CameraHint) => {
+            part.fillColor = 0x36ff14; // Green
+            part.setVisible(show);
+        });
+    }
+
+    private highlightCameraHint = (angle: number) => {
+        this.cameraHints.children.iterate((child: CameraHint) => {
+            if ((angle > child.StartDeg && angle < child.EndDeg) || (child.goToBounds && (angle < child.EndDeg || angle > child.StartDeg))){
+                child.setVisible(true);
+            }else {
+                child.setVisible(false);
+            }
+        })
+    }
+
+    private getAngleToTarget = (head: Phaser.GameObjects.Shape, target: Phaser.GameObjects.Shape): number => {
+        const angleRadian = Phaser.Math.Angle.Between(head.x, head.y, target.x, target.y);
+        let angleDeg = angleRadian * 180 / Math.PI
+
+        return angleDeg;
+    }
+
+    public updateHints = (playerHead: Phaser.GameObjects.Shape, target: Phaser.GameObjects.Shape) => {
+        
+        if (!this.objectInCameraViewport(target)){  
+            console.log('in viewport')
+            this.highlightCameraHint(this.getAngleToTarget(playerHead, target));
+        }else {
+            this.showCameraHints(false);
+        }
+    }
+}
+
+export default CameraManager;
--- src/snaketron/js/scenes/SceneMain.ts
+++ src/snaketron/js/scenes/SceneMain.ts
@@ -10,6 +10,7 @@ import Constants from "../../../toolbox/js/Constants";
 
 import backgroundImg from '../../images/background.jpg';
 import CameraHint from "../classes/CameraHint";
+import CameraManager from "../classes/CameraManager";
 
 class SceneMain extends Phaser.Scene {
 
@@ -22,8 +23,7 @@ class SceneMain extends Phaser.Scene {
     private cursorKeys: Phaser.Types.Input.Keyboard.CursorKeys;
     private keyboardInput_H: Phaser.Input.Keyboard.Key;
     private scoreBox: ScoreBox;
-
-    private cameraHints: Phaser.GameObjects.Group;
+    private cameraManager: CameraManager;
 
     private gameSpeed: number = 100; // ms between moving the player
 
@@ -68,9 +68,8 @@ class SceneMain extends Phaser.Scene {
         // Cameras
         this.cameras.main.setBounds(0, 0, +this.game.config.width, +this.game.config.height);
         
-        // Add camera hints before we start moving the camera around with 'follow'
-        this.cameraHints = this.add.group();
-        this.addCameraHints();
+        // Setup CameraManager
+        this.cameraManager = new CameraManager({scene: this}, this.cameras.main);
 
         this.cameras.main.startFollow(this.player.head, true);
         this.cameras.main.setLerp(0.1, 0.1)
@@ -101,12 +100,8 @@ class SceneMain extends Phaser.Scene {
             this.checkPlayerCollision();
 
             this.addPendingFood();
-            this.getAngleToFood()
-            if (!this.objectInCameraViewport(this.food)){
-                this.highlightCameraHint(this.getAngleToFood());
-            }else {
-                this.showCameraHints(false);
-            }
+            this.cameraManager.updateHints(this.player.head, this.food);
+            
         }
 
         // User can tell it to change direction whenever they want
@@ -138,169 +133,7 @@ class SceneMain extends Phaser.Scene {
             this.shouldAddFood = true;
         }
     }
-
-    // TODO:- Make generic
-    private objectInCameraViewport = (obj: Food): boolean => {
-        const x = this.cameras.main.midPoint.x - 250;
-        const width = this.cameras.main.midPoint.x + 250;
-        
-        const y = this.cameras.main.midPoint.y - 250;
-        const height = this.cameras.main.midPoint.y + 250;
-
-        const bounds = new Phaser.Geom.Rectangle(x, y, width, height);
-        return bounds.contains(obj.x, obj.y);
-    }
-
-    private addCameraHints = () => {
-
-        const hints = [
-        { // TR Corner Horizontal
-            x: 400,
-            y: 0,
-            width: 100,
-            height: 5,
-            alpha: 0.5,
-            startDeg: -60,
-            endDeg: -30
-        },
-        { // TR Corner Vertical
-            x: 495,
-            y: 0,
-            width: 5,
-            height: 100,
-            alpha: 0.5,
-            startDeg: -60,
-            endDeg: -30
-        },
-        { // Right
-            x: 495,
-            y: 150,
-            width: 5,
-            height: 200,
-            alpha: 0.5,
-            startDeg: -30,
-            endDeg: 30
-        },
-        { // BR Corner Horizontal
-            x: 400,
-            y: 495,
-            width: 100,
-            height: 5,
-            alpha: 0.5,
-            startDeg: 30,
-            endDeg: 60
-        },
-        { // BR Corner Vertical
-            x: 495,
-            y: 400,
-            width: 5,
-            height: 100,
-            alpha: 0.5,
-            startDeg: 30,
-            endDeg: 60
-        },
-        { // Bottom
-            x: 150,
-            y: 495,
-            width: 200,
-            height: 5,
-            alpha: 0.5,
-            startDeg: 60,
-            endDeg: 120
-        },
-        { // BL Corner Horizontal
-            x: 0,
-            y: 495,
-            width: 100,
-            height: 5,
-            alpha: 0.5,
-            startDeg: 120,
-            endDeg: 150
-        },
-        { // BL Corner Vertical
-            x: 0,
-            y: 400,
-            width: 5,
-            height: 100,
-            alpha: 0.5,
-            startDeg: 120,
-            endDeg: 150
-        },
-        { // Left
-            x: 0,
-            y: 150,
-            width: 5,
-            height: 200,
-            alpha: 0.5,
-            startDeg: 150, // TODO:- 
-            endDeg: -150, // TODO:- 
-            bounds: true
-        },
-        {
-            // TL Corner Horizontal
-            x: 0,
-            y: 0,
-            width: 100,
-            height: 5,
-            alpha: 0.5,
-            startDeg: -150,
-            endDeg: -120
-        },{
-            // TL Corner Vertical
-            x: 0,
-            y: 0,
-            width: 5,
-            height: 100,
-            alpha: 0.5,
-            startDeg: -150,
-            endDeg: -120
-        },
-        { // Top
-            x: 150,
-            y: 0,
-            width: 200,
-            height: 5,
-            alpha: 0.5,
-            startDeg: -120,
-            endDeg: -60
-        }];
-
-        hints.forEach(hint => {
-            const hintRect = this.add.rectangle(hint.x, hint.y, hint.width, hint.height, 0x36ff14, 0.75).setScrollFactor(0).setOrigin(0, 0) as CameraHint;
-            hintRect.StartDeg = hint.startDeg;
-            hintRect.EndDeg = hint.endDeg;
-            hintRect.goToBounds = hint.bounds;
-            hintRect.setVisible(false);
-            this.cameraHints.add(hintRect);
-        });
-    }
-
-    private showCameraHints = (show: boolean) => {
-        this.cameraHints.children.iterate((part: CameraHint) => {
-            part.fillColor = 0x36ff14; // Green
-            part.setVisible(show);
-        });
-    }
-
-    private highlightCameraHint = (angle: number) => {
-        this.cameraHints.children.iterate((child: CameraHint) => {
-            if ((angle > child.StartDeg && angle < child.EndDeg) || (child.goToBounds && (angle < child.EndDeg || angle > child.StartDeg))){
-                child.setVisible(true);
-            }else {
-                child.setVisible(false);
-            }
-        })
-    }
-
-    private getAngleToFood = (): number => {
-        const head = this.player.head;
-        const food = this.food;
-        const angleRadian = Phaser.Math.Angle.Between(head.x, head.y, food.x, food.y);
-        let angleDeg = angleRadian * 180 / Math.PI
-
-        return angleDeg;
-    }
-
+    
     private checkPlayerCollision = () => {
         const headIndex = this.player.currentPlayersHeadPosition;
 
